77f5c389238c78d6b01535a6a8bb75df52768371
anthonyfuller <itpartners12@gmail.com>
2026-01-31 18:11:47 -0500

    Update __init__.py
    fix(policy): align public exports with actual BasePEP and PolicyDecision types

--- src/policy/__init__.py
+++ src/policy/__init__.py
@@ -1,7 +1,8 @@
-from .pep_core import PolicyEnforcementPoint
+from .pep_core import BasePEP, PolicyDecision
 from .pep_opa import OPAPolicyEngine
 
 __all__ = [
-    "PolicyEnforcementPoint",
+    "BasePEP",
+    "PolicyDecision",
     "OPAPolicyEngine",
 ]
