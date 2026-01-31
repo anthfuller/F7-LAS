f17631d48603cad608568673ddef2a6eee052f7e
anthonyfuller <itpartners12@gmail.com>
2026-01-31 18:28:16 -0500

    Update __init__.py
    fix(policy): export OPAPEP instead of nonexistent OPAPolicyEngine

--- src/policy/__init__.py
+++ src/policy/__init__.py
@@ -1,8 +1,8 @@
 from .pep_core import BasePEP, PolicyDecision
-from .pep_opa import OPAPolicyEngine
+from .pep_opa import OPAPEP
 
 __all__ = [
     "BasePEP",
     "PolicyDecision",
-    "OPAPolicyEngine",
+    "OPAPEP",
 ]
